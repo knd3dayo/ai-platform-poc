6c5b5821f0fc8beee33a792510a9fcae51bef1b7
user <user@DESKTOP-QK8T16E>
2026-03-22 10:28:09 +0900

    update

--- test/application/agent_docker/run_client.sh
+++ test/application/agent_docker/run_client.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export LLM_API_KEY=sk-poc-master-key-12345
 
-uv run -m ai_chat_util.cli --loglevel INFO --logfile chat_timeout_5s.log chat --use_mcp -p "自律側エージェントMCPツールで、/srv/ai_platform/workspaces/e2e_sv_ws_1/done.txtに「完了2」と書き込んで.最後にget_resultでログを出力して"
+uv run -m ai_chat_util.cli --loglevel INFO --logfile chat_timeout_5s.log chat --use_mcp -p "自律側エージェントMCPツールで、/home/user/data/workspace/e2e_sv_ws_1/done.txtに「完了2」と書き込んで.最後にget_resultでログを出力して"
 
-uv run -m ai_chat_util.cli --loglevel INFO --logfile chat_timeout_5s.log chat --use_mcp -p "/srv/ai_platform/workspaces/e2e_sv_ws_1/をワークスペースとしてください。 自律側エージェントMCPツールで、ワークスペース内のファイル一覧を表示して。最後にget_resultでログを出力して"
+# uv run -m ai_chat_util.cli --loglevel INFO --logfile chat_timeout_5s.log chat --use_mcp -p "/srv/ai_platform/workspaces/e2e_sv_ws_1/をワークスペースとしてください。 自律側エージェントMCPツールで、ワークスペース内のファイル一覧を表示して。最後にget_resultでログを出力して"
 
-cat /srv/ai_platform/workspaces/e2e_sv_ws_1/done.txt
+cat /home/user/data/workspace/e2e_sv_ws_1/done.txt
